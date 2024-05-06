c2659266a4700014d477939cfb6245f602496b28
Paul Tee <jianhaoti@gmail.com>
2024-05-06 14:15:21 -0400

    update webpack and dist

--- dist/content.js
+++
@@ -1,1 +0,0 @@
-(()=>{var e=window.location.href,o=(new Date).toISOString();chrome.storage.session.set({lastVisit:{url:e,time:o}},(function(){console.log("URL and timestamp have been stored in session storage.")}))})();
--- webpack.config.js
+++ webpack.config.js
@@ -4,25 +4,31 @@ const path = require("path");
 
 module.exports = {
   entry: {
-    background: "./src/background.js", // Background script
-    content: "./src/content.js", // Content script
-    options: "./src/options.js", // Options script, if you have separate JS for options
+    background: "./src/background.js",
+    softblock: "./src/softblock.js",
+    options: "./src/options.js",
+    calendar: "./src/calendar.js",
   },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name].js",
+    filename: "[name].js", // This should create 'softblock.js' correctly
   },
   plugins: [
+    // calendar
+    new HtmlWebpackPlugin({
+      template: "./src/calendar.html",
+      filename: "calendar.html",
+      chunks: ["calendar"],
+    }),
+
+    // options
     new HtmlWebpackPlugin({
       template: "./src/options.html",
       filename: "options.html",
       chunks: ["options"], // Include only 'options' chunk if you have specific JS for options page
     }),
     new CopyPlugin({
-      patterns: [
-        { from: "src/manifest.json", to: "manifest.json" },
-        // { from: "src/rules.json", to: "rules.json" },
-      ],
+      patterns: [{ from: "src/manifest.json", to: "manifest.json" }],
     }),
   ],
   module: {
