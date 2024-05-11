79f661599c42997bdef559dec03234628f3fcfba
noodlewavey <117111580+noodlewavey@users.noreply.github.com>
2024-05-11 17:05:33 -0400

    softblock page launched useeffect is executig, but nothing is rendering

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Softblock from './softblock'; // Ensure this imports correctly
+import Softblock from './softblock.js'; // Ensure this imports correctly
 
 function App() {
   return (
