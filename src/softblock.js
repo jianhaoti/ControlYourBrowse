7c3b4b2b81da89e58528ee5fc672b7c805a99c7f
noodlewavey <117111580+noodlewavey@users.noreply.github.com>
2024-05-11 19:46:05 -0400

    commenting chrome.storage.local.get out makes the page render...now i just need to fix the bug! but renders with commenting out

--- src/softblock.js
+++ src/softblock.js
@@ -10,17 +10,17 @@ function Softblock() {
     // Function to handle the fetching and setting of the intercepted URL
     function fetchAndSetInterceptedUrl() {
       console.log("we're going into the function")
-      chrome.storage.local.get("interceptedURL", (data) => {
-        if (data.interceptedURL) {
-          console.log("Received intercepted URL from storage:", data.interceptedURL);
-          setInterceptedUrl(data.interceptedURL); // Set state
+      // chrome.storage.local.get("interceptedURL", (data) => {
+      //   if (data.interceptedURL) {
+      //     console.log("Received intercepted URL from storage:", data.interceptedURL);
+      //     setInterceptedUrl(data.interceptedURL); // Set state
         
-          // Optionally clear the URL from storage after retrieving it
-          chrome.storage.local.remove("interceptedURL", () => {
-            console.log("Intercepted URL cleared from storage.");
-          });
-        }
-      });
+      //     // Optionally clear the URL from storage after retrieving it
+      //     chrome.storage.local.remove("interceptedURL", () => {
+      //       console.log("Intercepted URL cleared from storage.");
+      //     });
+      //   }
+      // });
     }
 
     // Call the function immediately upon component mount
