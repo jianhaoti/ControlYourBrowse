3425cc44b8df33730921b0a538238b6442415e01
Paul Tee <jianhaoti@gmail.com>
2024-05-11 19:47:05 -0400

    Update softblock.js

--- src/softblock.js
+++ src/softblock.js
@@ -9,23 +9,22 @@ function Softblock() {
 
     // Function to handle the fetching and setting of the intercepted URL
     function fetchAndSetInterceptedUrl() {
-      console.log("we're going into the function")
-      chrome.storage.local.get("interceptedURL", (data) => {
-        if (data.interceptedURL) {
-          console.log("Received intercepted URL from storage:", data.interceptedURL);
-          setInterceptedUrl(data.interceptedURL); // Set state
-        
-          // Optionally clear the URL from storage after retrieving it
-          chrome.storage.local.remove("interceptedURL", () => {
-            console.log("Intercepted URL cleared from storage.");
-          });
-        }
-      });
+      console.log("we're going into the function");
+      // chrome.storage.local.get("interceptedURL", (data) => {
+      //   if (data.interceptedURL) {
+      //     console.log("Received intercepted URL from storage:", data.interceptedURL);
+      //     setInterceptedUrl(data.interceptedURL); // Set state
+
+      //     // Optionally clear the URL from storage after retrieving it
+      //     chrome.storage.local.remove("interceptedURL", () => {
+      //       console.log("Intercepted URL cleared from storage.");
+      //     });
+      //   }
+      // });
     }
 
     // Call the function immediately upon component mount
     fetchAndSetInterceptedUrl();
-
   }, []); // Empty dependency array to mimic componentDidMount
 
   // Rendering the component with the intercepted URL
