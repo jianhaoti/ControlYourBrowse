a6b231e2f54c56e9bb71a62a42058aa72774b7b8
noodlewavey <117111580+noodlewavey@users.noreply.github.com>
2024-05-12 13:08:56 -0400

    superficial changes....themeprovider works, but theme isnt set yet! no bugs so far

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Softblock from './softblock.js'; // Ensure this imports correctly
+import Softblock from './softblock.js'; // Ensure this imports correctly]8
+import theme from './theme.js';
+import { ThemeProvider } from 'styled-components';
+import { Theme } from '@fullcalendar/core/internal';
+
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <Router>
       <Routes>
         <Route path="/softblock" element={<Softblock />} />
         {/* Add more routes as needed */}
       </Routes>
     </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,30 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    background: {
+      default: '#e0ceca',
+      paper: '#e0ceca', // optional, used for surfaces of components such as Paper and Card
+    },
+    text: {
+      primary: '#333333', // Dark enough for primary text
+      secondary: '#575757', // Lighter than primary text
+      disabled: '#aaaaaa' // Optional: for disabled text
+    }
+  },
+  typography: {
+    fontFamily: [
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif'
+    ].join(','),
+  }
+});
+
+export default theme;
