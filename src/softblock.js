fd6c5938f7b207863ceb275e379061cd401c48cd
Paul Tee <jianhaoti@gmail.com>
2024-05-11 20:02:04 -0400

    Update softblock.js

--- src/softblock.js
+++ src/softblock.js
@@ -4,26 +4,35 @@ function Softblock() {
   const [interceptedUrl, setInterceptedUrl] = useState("");
 
   useEffect(() => {
-    // DOMContentLoaded equivalent in React using useEffect with an empty dependency array
-    console.log("Softblock page launched 12345");
+    // Check if the Chrome API is available
+    if (chrome.storage) {
+      console.log("Softblock page launched 12345");
 
-    // Function to handle the fetching and setting of the intercepted URL
-    function fetchAndSetInterceptedUrl() {
-      console.log("we're going into the function");
-      // chrome.storage.local.get("interceptedURL", (data) => {
-      //   if (data.interceptedURL) {
-      //     console.log("Received intercepted URL from storage:", data.interceptedURL);
-      //     setInterceptedUrl(data.interceptedURL); // Set state
-      //     // Optionally clear the URL from storage after retrieving it
-      //     chrome.storage.local.remove("interceptedURL", () => {
-      //       console.log("Intercepted URL cleared from storage.");
-      //     });
-      //   }
-      // });
+      // Function to handle the fetching and setting of the intercepted URL
+      function fetchAndSetInterceptedUrl() {
+        console.log("we're going into the function");
+
+        // Accessing chrome.storage.local
+        chrome.storage.local.get("interceptedURL", (data) => {
+          if (data.interceptedURL) {
+            console.log(
+              "Received intercepted URL from storage:",
+              data.interceptedURL
+            );
+            setInterceptedUrl(data.interceptedURL); // Set state
+            // Optionally clear the URL from storage after retrieving it
+            chrome.storage.local.remove("interceptedURL", () => {
+              console.log("Intercepted URL cleared from storage.");
+            });
+          }
+        });
+      }
+
+      // Call the function immediately upon component mount
+      fetchAndSetInterceptedUrl();
+    } else {
+      console.error("Chrome API is not available");
     }
-
-    // Call the function immediately upon component mount
-    fetchAndSetInterceptedUrl();
   }, []); // Empty dependency array to mimic componentDidMount
 
   // Rendering the component with the intercepted URL
