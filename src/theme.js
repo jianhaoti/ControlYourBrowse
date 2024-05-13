499bc86b28465ac4864d991ef23ae750020254e3
noodlewavey <117111580+noodlewavey@users.noreply.github.com>
2024-05-13 19:46:03 -0400

    vollkorn added! ta-da works

--- src/theme.js
+++ src/theme.js
@@ -10,21 +10,34 @@ const theme = createTheme({
     },
     background: {
       default: '#e0ceca',
-      paper: '#e0ceca', // optional, used for surfaces of components such as Paper and Card
+      paper: '#e0ceca',
     },
     text: {
-      primary: '#333333', // Dark enough for primary text
-      secondary: '#575757', // Lighter than primary text
-      disabled: '#aaaaaa' // Optional: for disabled text
+      primary: '#333333',
+      secondary: '#575757',
+      disabled: '#aaaaaa'
     }
   },
   typography: {
-    fontFamily: [
-      'Times New Roman',
-      'Arial',
-      'sans-serif'
-    ].join(','),
+    fontFamily: 'Vollkorn, serif',
+    h1: {
+      fontSize: '2.5rem', // Example size, adjust as needed
+      fontWeight: 500, // You can define the weight
+      lineHeight: 1.2, // Adjust line height as needed
+    },
+    h2: {
+      fontSize: '2.25rem',
+      fontWeight: 500,
+      lineHeight: 1.3,
+    },
+    h4: {
+      fontSize: '2rem',
+      fontWeight: 500,
+      lineHeight: 1.35,
+    },
+    // Continue with other styles for h4, h5, etc.
   }
 });
+
 
 export default theme;
