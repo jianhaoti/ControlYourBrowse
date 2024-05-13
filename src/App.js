5acc05f778a799be02ac277ed609bf3328d4b825
noodlewavey <117111580+noodlewavey@users.noreply.github.com>
2024-05-13 13:00:15 -0400

    now i refactored scheudle to be a react component...this works! i still just need to fix themeprovider
    this works

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Softblock from './softblock.js'; // Ensure this imports correctly
+import Schedule from './schedule.js';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/softblock" element={<Softblock />} />
         {/* Add more routes as needed */}
+        <Route path="/schedule" element={<Schedule />} />
       </Routes>
     </Router>
   );
